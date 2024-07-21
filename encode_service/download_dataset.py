9c46c2830619e334dc9253638ea0de32e238d193
meredithoopis <nthhanh.work.01@gmail.com>
2024-07-21 17:47:05 +0700

    final

--- encode_service/download_dataset.py
+++ encode_service/download_dataset.py
@@ -1,4 +1,13 @@
-import roboflow
-roboflow.login()
+from huggingface_hub import hf_hub_download
+import zipfile
 
-roboflow.download_dataset(dataset_url="https://universe.roboflow.com/team-roboflow/coco-128/dataset/2", model_format="coco", location="data/coco-128")
+data_path = 'data'
+
+download_path = hf_hub_download(repo_id='huudung141434/coco128', 
+                                    filename='COCO-128-2.zip', repo_type='space',
+                                   local_dir=data_path)
+print(f"File downloaded to {download_path}")
+    
+with zipfile.ZipFile(download_path, 'r') as zip_ref:
+        zip_ref.extractall(data_path)
+print(f"Files extracted to {data_path}")
